1b15a291b6d339a32811f7797263ccf986bb2d86
Fahim Montasir Turza <turzacse@gmail.com>
2024-10-29 23:28:21 +0600

    update the post API

--- index.js
+++ index.js
@@ -237,14 +237,24 @@ async function run() {
     app.post('/contact-us', async (req, res) => {
       const enquiry = {
         ...req.body,
-        createdAt: new Date() // Add createdAt field with current date and time
+        createdAt: new Intl.DateTimeFormat('en-GB', {
+          timeZone: 'Asia/Dhaka',
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: false
+        }).format(new Date()) // Format date in dd-mm-yyyy hr:mm:ss in BST
       };
-      
+    
       console.log(enquiry);
-      
+    
       const result = await ContactCollection.insertOne(enquiry);
       res.send(result);
     });
+    
     
     app.get('/contact-us', async (req, res) => {
       console.log(req.body);
